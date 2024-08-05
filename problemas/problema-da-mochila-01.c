3dc17bc49d2e126466e8d9f3d2ba500283b9c85c
Isaias00C <167316633+Isaias00C@users.noreply.github.com>
2024-08-05 17:30:37 -0300

    Update problema-da-mochila-01.c

--- problemas/problema-da-mochila-01.c
+++ problemas/problema-da-mochila-01.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int p[100], v[100];
+int p[100], v[100], memo[100][100], booleana[100];
 int mochila(int pesoMax, int t);
 int max(int a, int b);
 
@@ -11,6 +11,20 @@ int main(){
     printf("capacidade: ");
     scanf("%d", &capacidade);
 
+    //preparando o memo
+    for(int i = 0; i <= capacidade; i++){
+        memo[capacidade][i] = 0;
+    }
+    for(int i = 0; i <= tamanho; i++){
+        memo[i][tamanho] = 0;
+    }
+    for(int i = 1; i <= capacidade; i++){
+        for(int j = 1; j <= tamanho; j++){
+            memo[i][j] = -1;
+        }
+    }
+    
+    
     printf("vetor de pesos: ");
     for(int i = 0; i < tamanho; i++){
         scanf("%d", &p[i]);
@@ -29,12 +43,18 @@ int max(int a, int b){
 }
 
 int mochila(int pesoMax, int t){
-    if(pesoMax == 0  || t == 0) return 0;
+    if(memo[pesoMax][t] == -1){
+        if(pesoMax == 0  || t == 0) return 0;
 
-    if(p[t-1] > pesoMax) mochila(pesoMax, t-1);
-    else{
-        int incluindo = v[t-1] + mochila(pesoMax - v[t-1], t-1);
-        int nao_incluindo = mochila(pesoMax, t-1);
-        return max(incluindo, nao_incluindo);
+        if(p[t-1] > pesoMax) mochila(pesoMax, t-1);
+        else{
+            int incluindo = v[t-1] + mochila(pesoMax - p[t-1], t-1);
+            int nao_incluindo = mochila(pesoMax, t-1);
+            
+            memo[pesoMax][t] = max(incluindo, nao_incluindo);
+            return max(incluindo, nao_incluindo);
+        }
+    }else{
+        return memo[pesoMax][t];
     }
 }
